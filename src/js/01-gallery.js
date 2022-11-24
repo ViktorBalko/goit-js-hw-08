af1258128dbf57893dee801f90df7ebfdabd3a42
ViktorBalko <viktorbalko.samsung@gmail.com>
2022-11-24 21:48:18 +0200

    24.11
    +gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,16 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
-console.log('helloWorld');
-console.log('роби нормально! - нормально буде :)');
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// механізм пуша на --> gitHub
-// після всіх змін, зупиняємо проект --> ctrl + c
-// можна перевірити зміни --> git status (виділення змінених файлів буде червоним кольором)
-// додаємо файли в комміт --> git add . (крапка означає що ми додаємо всі файли зі змінами. Можна обирати поодинці)
-// перевіряємо додавання --> git status (виділення змінених файлів буде зеленим кольором)
-// створюємо комміт і пишемо коментарій (по аналогії з gitHub Desktop) --> git commit -m "назва комміту"
-// перевіряємо чи все йде як потрібно очікуючи "nothing to commit, work tree clean" -->  git status
-// ну, і власне, пушимо --> git push 
+const galleryList = document.querySelector(".gallery");
+const galleryCreate = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryCreate);
+
+function createGalleryMarkup(elements) {
+                 return elements.map(({ preview, original, description }) => {
+                                  return `<a class="gallery__item" href='${original}'>
+        <img class="gallery__image"
+          src='${preview}'
+          alt='${description}'
+        />
+      </a>
+    `;
+                 }).join("");
+};
+
+const lightbox = new SimpleLightbox(".gallery a",
+                 {
+                                  captionsData: `alt`,
+                                  captionDelay: 250,
+                 });
