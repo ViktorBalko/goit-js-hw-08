663d7440dd5a898d3c0fe1de0d53f9c69f47ab0e
ViktorBalko <viktorbalko.samsung@gmail.com>
2022-11-26 01:15:25 +0200

    26.11
    +vimeoPlayer
    +feedbackForm

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const inputMail = document.querySelector("input[name = 'email']");
+const inputMessage = document.querySelector("textarea[name = 'message']");
+const buttonSubmit = document.querySelector('button[type = "submit"]');
+
+feedbackForm.addEventListener('input', throttle(onTyping, 500));
+feedbackForm.addEventListener('submit', onSubmit);
+dataRecovery();
+
+function onTyping() {
+                 const data = { email: inputMail.value, message: inputMessage.value };
+
+                 localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+function dataRecovery() {
+                 const data = localStorage.getItem(STORAGE_KEY);
+                 const parsedData = JSON.parse(data);
+                 if (data) {
+                                  inputMail.value = parsedData.email;
+                                  inputMessage.value = parsedData.message;
+                 }
+}
+
+function onSubmit(e) {
+                 e.preventDefault();
+                 console.log({ email: inputMail.value, message: inputMessage.value });
+                 e.currentTarget.reset();
+
+                 localStorage.removeItem(STORAGE_KEY);
+};
