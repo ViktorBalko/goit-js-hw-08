ba721c7644eb4bbcee4c3e233bbf377014a71918
ViktorBalko <viktorbalko.samsung@gmail.com>
2022-11-26 02:38:55 +0200

    26.11
    finally

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,21 +8,17 @@ const TIME_KEY = 'videoplayer-current-time';
 
 
 player.on(
-                 'timeupdate',
-                 throttle(function (data) {
-                                  localStorage.setItem(TIME_KEY, data.seconds);
-                 }, 1000),
+    'timeupdate',
+    throttle(function (data) {
+        localStorage.setItem(TIME_KEY, data.seconds);
+    }, 1000),
 );
 player.setCurrentTime(localStorage.getItem(TIME_KEY)).then(function (seconds) {
-                 // seconds = the actual time that the player seeked to
 }).catch(function (error) {
-                 switch (error.name) {
-                                  case 'RangeError':
-                                                   // the time was less than 0 or greater than the video’s duration
-                                                   break;
-
-                                  default:
-                                                   // some other error occurred
-                                                   break;
-                 }
-}); X
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,24 +12,24 @@ feedbackForm.addEventListener('submit', onSubmit);
 dataRecovery();
 
 function onTyping() {
-                 const data = { email: inputMail.value, message: inputMessage.value };
+    const data = { email: inputMail.value, message: inputMessage.value };
 
-                 localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 }
 
 function dataRecovery() {
-                 const data = localStorage.getItem(STORAGE_KEY);
-                 const parsedData = JSON.parse(data);
-                 if (data) {
-                                  inputMail.value = parsedData.email;
-                                  inputMessage.value = parsedData.message;
-                 }
+    const data = localStorage.getItem(STORAGE_KEY);
+    const parsedData = JSON.parse(data);
+    if (data) {
+        inputMail.value = parsedData.email;
+        inputMessage.value = parsedData.message;
+    }
 }
 
 function onSubmit(e) {
-                 e.preventDefault();
-                 console.log({ email: inputMail.value, message: inputMessage.value });
-                 e.currentTarget.reset();
+    e.preventDefault();
+    console.log({ email: inputMail.value, message: inputMessage.value });
+    e.currentTarget.reset();
 
-                 localStorage.removeItem(STORAGE_KEY);
-};
+    localStorage.removeItem(STORAGE_KEY);
+}
